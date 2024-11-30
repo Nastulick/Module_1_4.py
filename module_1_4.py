d6d53913153713a7a573a9795a10466edb13361d
Nastulick <nastya9031976011@gmail.com>
2024-11-30 18:27:14 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,15 @@
+my_string=input('Я изучаю программирование ')
+print(len(my_string))
+print('в университете Urban.'.upper())
+print('в университете Urban.'.lower())
+print('в университете Urban.'.replace(' ',""))
+
+a='в'
+c='в университете Urban'
+if a == c[0] :
+    print('первая буква: в')
+
+b='n'
+d='в университете Urban'
+if b== d[-1]:
+    print('последняя буква: n')
